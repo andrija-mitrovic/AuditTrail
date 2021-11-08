ce8cfed036567dc229cc5cd09182090efa26f957
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-11-08 15:56:49 +0100

    ApplicationDbContext added

---
+++ AuditTrail/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuditTrail.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuditTrail.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
